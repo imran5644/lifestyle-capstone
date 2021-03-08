ffd6f349a3ac6ded647290a2d6c251b04bc07ba5
imran5644 <imi56444@gmail.com>
2021-03-09 04:06:52 +0800

    updated the category dropdown list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create show]
-  root 'users#show'
+  root to: 'articles#index'
   get '/signup', to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users, only: [:new, :show, :update, :index, :create, :edit] do
+    resources :articles
+  end
+
+  resources :articles
+  resources :categories, only: [:show] do 
+  resources :articles
+end
+
 end
